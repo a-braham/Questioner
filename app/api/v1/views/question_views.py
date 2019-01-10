373825d1d1c9a6a8b2a35f385a3ac091f0d4ffe7
Abraham Kamau <kirumba.kamau@gmail.com>
2019-01-10 05:43:14 +0300

    [feature #163062273] added tests for upvotea and downvote and endpoints

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -12,7 +12,13 @@ questions = question_models.QuestionModel()
 def create_question():
     """ A view to control creation of question """
 
-    data = request.get_json()
+    try:
+        data = request.get_json()
+    except:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Wrong input"
+        })), 400
 
     title = data.get('title')
     body = data.get('body')
@@ -20,12 +26,60 @@ def create_question():
     createdby = data.get('createdby')
     votes = data.get('votes')
 
+    if not title:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Title cannot be empty"
+        })), 400
+
+    if not body:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Body cannot be empty"
+        })), 400
+    question = questions.create_question(
+            title, body, meetup, createdby, votes)
     return make_response(jsonify({
         "status": 201,
         "data": [{
             "title": title,
             "body": body,
             "meetup": meetup,
-            "createdby": createdby,
-            "votes": votes
+            "user": createdby,
         }]})), 201
+
+
+@question_bp.route('/<int:question_id>/upvote', methods=['PATCH'])
+def upvote(question_id):
+    """ Manipulates upvoting question """
+
+    question = questions.oneQuestion(question_id)
+    if question:
+        quiz=question[0]
+        quiz["votes"] = quiz["votes"] + 1
+        return make_response(jsonify({
+            "status": 200,
+            "data": quiz
+            })), 200
+    return make_response(jsonify({
+            "status": 404,
+            "message": "Question not found"
+            })), 404
+
+
+@question_bp.route('/<int:question_id>/downvote', methods=['PATCH'])
+def downvote(question_id):
+    """ Manipulates upvoting question """
+
+    question = questions.oneQuestion(question_id)
+    if question:
+        quiz=question[0]
+        quiz["votes"] = quiz["votes"] - 1
+        return make_response(jsonify({
+            "status": 200,
+            "data": quiz 
+            })), 200
+    return make_response(jsonify({
+            "status": 404,
+            "message": "Question not found"
+            })), 404
