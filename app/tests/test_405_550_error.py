c364809e5f1de1116deb216c10b79df2bbe1b349
Abraham Kamau <kirumba.kamau@gmail.com>
2019-01-15 15:32:59 +0300

    [starts #163222665] Added tests for edge cases

---
+++ app/tests/test_405_550_error.py
@@ -0,0 +1,28 @@
+import json, unittest, instance
+from app import create_app
+
+app = create_app("testing")
+
+class TestErrorCodes(unittest.TestCase):
+    """ Test error codes 404, 405 and 500 """
+    def setUp(self):
+        """ Test variables """
+
+        app.config.from_object(instance.config.Testing)
+        self.client = app.test_client()
+
+    def test_404(self):
+        response = self.client.post('/api/v1/user')
+        result = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(result['status'], 404)
+    
+    def test_405(self):
+        """ Test error 405 """
+
+        response = self.client.get('/api/v1/signup')
+        result = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(result['status'], 405)
