e280474bd81314a7e6a5c2bb89b8608118cfa082
Abraham Kamau <kirumba.kamau@gmail.com>
2019-01-16 19:16:43 +0300

    Modified database.py

--- app/database.py
+++ app/database.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def init_db():
     """ Method to initialize the database """
     with app.app_context():
-        conn = psycopg2.connect(dbname='Questioner', user='postgres', host='localhost', password='')
-        # url_db = Config.DATABASE_URL
-        # conn = psycopg2.connect(url_db)
+        # conn = psycopg2.connect(dbname='Questioner', user='postgres', host='localhost', password='')
+        url_db = Config.DATABASE_URL
+        conn = psycopg2.connect(url_db)
         cursor = conn.cursor()
         sql = current_app.open_resource('questioner.sql', mode='r')
         cursor.execute(sql.read())
@@ -25,8 +25,8 @@ def connect_to(url):
 def _init_db():
     """ Initialize database for test """
     with app.app_context():
-        conn = psycopg2.connect(dbname='Questioner_Test', user='postgres', host='localhost', password='')
-        # conn = connect_to(Testing.DATABASE_TEST_URL)
+        # conn = psycopg2.connect(dbname='Questioner_Test', user='postgres', host='localhost', password='')
+        conn = connect_to(Testing.DATABASE_TEST_URL)
         destroy_db()
         cursor = conn.cursor()
         sql_file = current_app.open_resource('questioner.sql', mode='r')
@@ -37,9 +37,9 @@ def _init_db():
 def destroy_db():
     """ Destroy database for test """
     with app.app_context():
-        conn = psycopg2.connect(dbname='Questioner_Test', user='postgres', host='localhost', password='')
-        # test_db_url = Testing.DATABASE_TEST_URL
-        # conn = connect_to(test_db_url)
+        # conn = psycopg2.connect(dbname='Questioner_Test', user='postgres', host='localhost', password='')
+        test_db_url = Testing.DATABASE_TEST_URL
+        conn = connect_to(test_db_url)
         cursor = conn.cursor()
         users = "DROP TABLE IF EXISTS users CASCADE"
         tables = [users]
