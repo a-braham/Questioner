f1c4704c402bd40e9312a9eef56f1ab693c8deee
Abraham Kamau <kirumba.kamau@gmail.com>
2019-01-16 21:35:37 +0300

    Modifying travis

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export DATABASE_NAME="Questioner"
 export DATABASE_USER="postgres"
-export TEST_DATABASE = "Questioner_Test"
-export SECRET_KEY = "abraham"
+# export TEST_DATABASE = "Questioner_Test"
+# export SECRET_KEY = "abraham"
 export DATABASE_HOST="localhost"
 export DATABASE_PASSWORD=""
 export ENVIRONMENT="DEVELOPMENT"
