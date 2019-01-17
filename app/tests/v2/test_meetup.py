6ebd350ab6c909b05e9c35c2674a218dc8a2e80b
Abraham Kamau <kirumba.kamau@gmail.com>
2019-01-18 01:07:53 +0300

    [finish #163142225] Modified tests to run successfully

--- app/tests/v2/test_meetup.py
+++ app/tests/v2/test_meetup.py
@@ -1,4 +1,5 @@
-import unittest, token
+import unittest
+import token
 import json
 import instance
 from app.api.v1.views import meetup_views
@@ -8,6 +9,7 @@ from app.database import _init_db, destroy_db
 
 app = create_app("testing")
 users = user_models.UserModel()
+
 
 class TestMeetup(unittest.TestCase):
     """Test class for meetup views """
@@ -30,34 +32,35 @@ class TestMeetup(unittest.TestCase):
         }
 
         self.u_register = {
-             "firstname": "Abraham",
-             "lastname": "Kirumba",
-             "othername": "Kamau",
-             "email": "eric@gmail.com",
-             "phoneNumber": "123456789",
-             "isAdmin": "True",
-             "username": "Kamaa",
-             "password": "ak?,T4.jj12kjn@"
+            "firstname": "Abraham",
+            "lastname": "Kirumba",
+            "othername": "Kamau",
+            "email": "eric@gmail.com",
+            "phoneNumber": "123456789",
+            "isAdmin": "True",
+            "username": "Kamaa",
+            "password": "ak?,T4.jj12kjn@"
         }
 
         self.user = {
-             "username": "Kamaa",
-             "password": "ak?,T4.jj12kjn@"
+            "username": "Kamaa",
+            "password": "ak?,T4.jj12kjn@"
         }
         self.test_DB = _init_db()
 
     def test_create_meetup(self):
         """ Test creation of meetup """
-        user = users.login(self.user["username"])
-        if not user:
-            response = self.client.post(
-                "/api/v2/signup", data=json.dumps(self.user), content_type="application/json"
-            )
-        user = self.client.post(
+        self.client.post(
+            "/api/v2/signup", data=json.dumps(self.u_register), content_type="application/json"
+        )
+        user_resp = self.client.post(
             "/api/v2/login", data=json.dumps(self.user), content_type="application/json")
+        result = json.loads(user_resp.data.decode('utf-8'))
+        token = 'Bearer ' + result["token"]
+        header = {"Authorization": token}
         response = self.client.post(
-            "/api/v2/meetups", 
-            headers={"Authorization": 'Bearer ' + json.loads(user.data.decode('utf-8'))['token']}, 
+            "/api/v2/meetups",
+            headers=header,
             data=json.dumps(self.meetup), content_type="application/json")
         restult = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 201)
@@ -72,8 +75,8 @@ class TestMeetup(unittest.TestCase):
         ])
 
         response1 = self.client.post(
-            "/api/v2/meetups", 
-            headers={"Authorization": 'Bearer ' + json.loads(user.data.decode('utf-8'))['token']}, 
+            "/api/v2/meetups",
+            headers=header,
             data=json.dumps(self.meetup1), content_type="application/json")
         restult1 = json.loads(response1.data.decode('utf-8'))
         self.assertEqual(response1.status_code, 400)
@@ -83,17 +86,19 @@ class TestMeetup(unittest.TestCase):
     def test_view_meetup(self):
         """ Tests view posted meetups """
 
-        self.client.post("/api/v2/meetups", data=json.dumps(self.meetup), content_type="application/json")
-        response = self.client.get("/api/v2/meetups/upcoming", content_type="application/json")
+        self.client.post(
+            "/api/v2/meetups", data=json.dumps(self.meetup), content_type="application/json")
+        response = self.client.get(
+            "/api/v2/meetups/upcoming", content_type="application/json")
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(result["status"], 200)
-        
+
     def test_view_one_meetup(self):
         """ Tests view posted meetups """
 
-        self.client.post("/api/v2/meetups", data=json.dumps(self.meetup), content_type="application/json")
-        response = self.client.get("/api/v2/meetups/1", content_type="application/json")
+        response = self.client.get(
+            "/api/v2/meetups/1", content_type="application/json")
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(result["status"], 200)
@@ -101,8 +106,8 @@ class TestMeetup(unittest.TestCase):
     def test_create_rsvp(self):
         """ Tests view posted meetups """
 
-        self.client.post("/api/v2/meetups", data=json.dumps(self.meetup), content_type="application/json")
-        response = self.client.post("/api/v2/meetups/1/rsvps", data=json.dumps(self.rsvp), content_type="application/json")
+        response = self.client.post(
+            "/api/v2/meetups/1/rsvps", data=json.dumps(self.rsvp), content_type="application/json")
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 201)
         self.assertEqual(result["status"], 201)
